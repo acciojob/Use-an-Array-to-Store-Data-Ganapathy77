006671202e7db64ed87ac020d27aac90667b4db4
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-19 15:15:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let yourArray = [];
+yourArray.push("Hello");
+yourArray.push(1);
+yourArray.push(true);
+yourArray.push(1);
+yourArray.push("Bye");
 
+console.log("Your Array value is : " yourArray)
